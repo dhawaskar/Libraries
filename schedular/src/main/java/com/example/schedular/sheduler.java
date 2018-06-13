92cae64e0f4ed704caa4c3e35fd6060cc36b8077
sandesh <sadh0344@colorado.edu>
2018-06-13 12:14:45 -0600

    Creating the libraries

---
+++ schedular/src/main/java/com/example/schedular/sheduler.java
@@ -0,0 +1,15 @@
+package com.example.schedular;
+
+
+import android.util.Log;
+
+/**
+ * Created by sandesh on 6/12/2018.
+ */
+
+public class sheduler {
+    private static final  String tag="Welcome";
+    public  static void d(String message){
+        Log.d(tag,message);
+    }
+}
